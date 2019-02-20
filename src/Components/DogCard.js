2bee26804d20127bd22accdf93c524cd8eebfd27
reduviidae <wiljago@gmail.com>
2019-02-20 13:54:07 -0500

    milestone complete

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 class DogCard extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  ruff() {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-        <button onClick={""}>Bark</button>
+        <h2>{this.props.dog.name}</h2>
+        <div>{this.state.clicked ? <h2 className="ruff">RUFF!</h2> : ""}</div>
+        <img alt="" src={this.props.dog.img} />
+        <button onClick={this.ruff.bind(this)}>Bark</button>
       </div>
     );
   }
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import Dogs from "../dogs";
+import DogCard from "../Components/DogCard";
 
 class DogList extends Component {
+
+  state = {
+      dogs: Dogs
+  };
+
+
   render() {
-    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+    const dogCards = this.state.dogs.map(dog => <DogCard dog={dog} clicked={this.state.clicked}/>);
+    return <div className="dogContainer">{dogCards}</div>;
   }
 }
 
